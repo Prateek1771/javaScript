2ad69553a1e09e3c4761170b9fb2bed81fa3f2ae
Prateek Hitli <133594598+Prateek1771@users.noreply.github.com>
2024-09-04 05:38:46 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Prateek");
+
+let name = 'Prateek'
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => " "
+// boolean => true/false
+// null => data type, standalone value, empty value
+// undefined =>
+// symbol => unique 
+
+//object
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
