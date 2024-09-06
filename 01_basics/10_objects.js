23f9996337db7bbcacde906cf7ef09e182676992
Prateek Hitli <133594598+Prateek1771@users.noreply.github.com>
2024-09-06 02:12:21 +0000

    objects

--- 01_basics/10_objects.js
+++ 01_basics/10_objects.js
@@ -1,4 +1,5 @@
-// singleton
+// singleton          // const user1 = new Object()
+// non-singleton      // const user2 = {}
 // object.create
 // objects literals
 
@@ -14,3 +15,106 @@ const JsUser = {
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+// JsUser.email = "prateek@chatgpt.com"
+// Object.freeze(JsUser)
+// JsUser.email = "prateek@microsoft.com"
+// console.log(JsUser);
+// 
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+// // 
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+
+
+const user1 = {}
+
+user1.id = "123abc"
+user1.name = 'batman'
+user1.isLoggedIn = false
+
+// console.log(user1);       // { id: '123abc', name: 'batman', isLoggedIn: false }
+
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "prateek",
+            lastname: "hitli"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname); // prateek
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+// 
+// // const obj3 = { obj1, obj2 }
+// // const obj3 = Object.assign({}, obj1, obj2, obj4)
+// 
+// const obj3 = {...obj1, ...obj2}
+// // console.log(obj3);
+// 
+// 
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+// ]
+// 
+// users[1].email
+// // console.log(tinderUser);
+// 
+// // console.log(Object.keys(tinderUser));
+// // console.log(Object.values(tinderUser));
+// // console.log(Object.entries(tinderUser));
+// 
+// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// 
+// 
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+// 
+// // course.courseInstructor
+// 
+// const {courseInstructor: instructor} = course
+// 
+// // console.log(courseInstructor);
+// console.log(instructor);
+// 
+// // {
+// //     "name": "hitesh",
+// //     "coursename": "js in hindi",
+// //     "price": "free"
+// // }
+// 
+// [
+//     {},
+//     {},
+//     {}
+// ]
