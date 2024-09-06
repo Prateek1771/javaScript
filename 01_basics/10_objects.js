e7cf5c6b7692bbf60ee9ffbad6dc9c288dc0202a
Prateek Hitli <133594598+Prateek1771@users.noreply.github.com>
2024-09-06 08:56:37 +0000

    objects

--- 01_basics/10_objects.js
+++ 01_basics/10_objects.js
@@ -58,63 +58,50 @@ const regularUser = {
 
 console.log(regularUser.fullname.userfullname.firstname); // prateek
 
-// const obj1 = {1: "a", 2: "b"}
-// const obj2 = {3: "a", 4: "b"}
-// const obj4 = {5: "a", 6: "b"}
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
 // 
 // // const obj3 = { obj1, obj2 }
 // // const obj3 = Object.assign({}, obj1, obj2, obj4)
 // 
-// const obj3 = {...obj1, ...obj2}
-// // console.log(obj3);
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
 // 
-// 
-// const users = [
-//     {
-//         id: 1,
-//         email: "h@gmail.com"
-//     },
-//     {
-//         id: 1,
-//         email: "h@gmail.com"
-//     },
-//     {
-//         id: 1,
-//         email: "h@gmail.com"
-//     },
-// ]
-// 
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
 // users[1].email
-// // console.log(tinderUser);
-// 
-// // console.log(Object.keys(tinderUser));
-// // console.log(Object.values(tinderUser));
-// // console.log(Object.entries(tinderUser));
-// 
-// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-// 
-// 
-// const course = {
-//     coursename: "js in hindi",
-//     price: "999",
-//     courseInstructor: "hitesh"
-// }
-// 
-// // course.courseInstructor
-// 
-// const {courseInstructor: instructor} = course
-// 
-// // console.log(courseInstructor);
-// console.log(instructor);
-// 
-// // {
-// //     "name": "hitesh",
-// //     "coursename": "js in hindi",
-// //     "price": "free"
-// // }
-// 
-// [
-//     {},
-//     {},
-//     {}
-// ]
+// console.log(user1);
+// console.log(Object.keys(user1));                 // [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(user1));               // [ '123abc', 'batman', false ]
+// console.log(Object.entries(user1));              // [ [ 'id', '123abc' ], [ 'name', 'batman' ], [ 'isLoggedIn', false ] ]
+// console.log(user1.hasOwnProperty('isLoggedIn')); // true
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
