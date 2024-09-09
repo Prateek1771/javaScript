c403dd54b8478828b13250f84fe2171bd422f87d
Prateek Hitli <133594598+Prateek1771@users.noreply.github.com>
2024-09-09 10:01:32 +0000

    arrow

--- 01_basics/13_arrowfn.js
+++ 01_basics/13_arrowfn.js
@@ -16,8 +16,20 @@ const user = {
 // when we use console.log(this); on the code editor we will get empty object // the v8 engine is extracted and used saperately, renamed like node, bun
 // but when we do console.log(this); on the browser we will get window onject // runs within the browser
 
-function chai(){
+const chai = () => {
     let username = "hitesh"
-    console.log(this.username);
+    console.log(this);
 }
-chai()
+// chai()
+
+const add = (num1, num2) =>{
+    return num1 + num2
+} // Explicit return
+// console.log(add(5,10));
+
+const add2 = (num3, num4) => (num3 + num4)
+// Implicit return
+// console.log(add2(10,15));
+
+const add3 = (num5, num6) => ({username: 'Prateek'})
+console.log(add3(10,15));
