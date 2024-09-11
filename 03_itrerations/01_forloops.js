17acdcaa905d88faa29b932431a6e9455f2fdc90
Prateek1771 <hitliprateek5@gmail.com>
2024-09-11 18:51:42 +0530

    methods

--- 03_itrerations/01_forloops.js
+++ 03_itrerations/01_forloops.js
@@ -130,7 +130,7 @@ coding.forEach( function (val){
 } )
 
 const val = coding.forEach( (item) => {
-    console.log(item);
+//    console.log(item);
 } )
 
 // -------------------------
@@ -168,3 +168,110 @@ myCoding.forEach( (item) => {
 } )
 
 // --------------------------------
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums1 = myNumers.filter((num) => num > 4)
+// console.log(newNums1);
+ 
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+// console.log(newNums);
+// chaining 
+
+const newNums3 = []
+myNumers.forEach((num) => {
+    if(num > 4){
+        newNums3.push(num)
+    }
+}) 
+// console.log(newNums3);
+
+//-----------------------------------------------------
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+const myBooks = books.filter((fevbooks) => fevbooks.genre === 'Fiction')
+// console.log(myBooks);
+
+const myBooks2 = books.filter((fevbooks) => fevbooks.title == 'Book Nine')
+// console.log(myBooks2);
+
+const myBooks3 = books.filter((fevbooks) => fevbooks.publish >= 2000)
+//console.log(myBooks3);
+
+const myBooks4 = books.filter((fevbooks) => fevbooks.publish >= 2000 && fevbooks.genre == 'Science')
+//console.log(myBooks4);
+
+
+// ------------------------------------------------------
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const fevNum1 = myNum.map((num) => num + 10)
+// console.log(fevNum1);
+
+const fevNum2  = myNum.forEach((item) => {
+    return item * 10
+})
+// console.log(fevNum2);
+
+
+// ========================================================
+
+const arr1 = [1, 2, 3, 4]
+// 0 + 1 + 2 + 3 + 4
+const initialVal = 0;
+const sumWithInitial = arr1.reduce(
+    (accumulator , currentVal) => accumulator + currentVal, initialVal  
+)
+// accumulator => initial value is taken, after a loop the result(accumulator + currentVal) is taken as accumulator.
+
+// console.log(sumWithInitial);
+// 
+
+const myTotal = arr1.reduce(function (acc, currVal) {
+//    console.log(`accumulator: ${acc} and currentVal: ${currVal}`);
+    
+    return acc + currVal
+}, 0)
+
+//console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        item: 'Tooth brush',
+        price: 40,
+        stars: 4,
+    },
+    {
+        item: 'Mysore sandal soap',
+        price: 50,
+        stars: 5
+    },
+    {
+        item: 'Comfert',
+        price: 5,
+        stars: 4
+    }
+]
+
+const totalCost = shoppingCart.reduce(
+    (acc , initialCost) => acc + initialCost.price, 5
+)
+// 5 service sharge
+console.log(totalCost);
